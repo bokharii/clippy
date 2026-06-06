d00037a8185d93ec0c019ecee3de37b468b7afaf
sbokhari <bokharisaab12345@gmail.com>
2026-06-06 17:52:26 -0400

    start working oninput form

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -3,7 +3,11 @@ import type { Link } from "./types";
 import "./App.css";
 
 function App() {
-  const [isFormVisible, setFormVisible] = useState(false)
+  const [isFormVisible, setFormVisible] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    url: "",
+  });
   const [links, setLinks] = useState<Link[]>([
     {
       id: crypto.randomUUID(),
@@ -17,14 +21,46 @@ function App() {
     },
   ]);
   function handleClick() {
-    setFormVisible(prev => !prev)
+    setFormVisible((prev) => !prev);
+  }
+  function handleSubmit(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+  }
+  function handleFormChange(event) {
+    const { name, value } = event.content;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   }
 
   return (
     <>
       <h1>Clippy</h1>
-      <button onClick={handleClick}>Add Link</button>
-      {isFormVisible && <p>Hi</p>}
+      <button onClick={handleClick}>Add New Clip</button>
+      {isFormVisible && (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Clip Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            required
+            onChange={handleFormChange}
+          />
+          <label htmlFor="url">Clip Content</label>
+          <input
+            type="text"h
+            id="url"
+            name="url"
+            value={formData.url}
+            required
+            onChange={handleFormChange}
+          />
+          <button>Create</button>
+        </form>
+      )}
       {links.map((link) => (
         <div key={link.id} className="link-row">
           {link.name}: {link.url}
